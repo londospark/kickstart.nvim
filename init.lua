2d4ea336c9479ff5f6982b4f42780b32fbd8ddab
Londo <ridecar2@gmail.com>
2025-05-26 02:57:31 +0100

    Readd Noice

--- init.lua
+++ init.lua
@@ -777,7 +777,26 @@ require('lazy').setup(
     {
       'saghen/blink.cmp',
       -- optional: provides snippets for the snippet source
-      dependencies = { 'rafamadriz/friendly-snippets' },
+      dependencies = {
+        'rafamadriz/friendly-snippets',
+
+        -- lazy.nvim
+        {
+          'folke/noice.nvim',
+          event = 'VeryLazy',
+          opts = {
+            -- add any options here
+          },
+          dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            'MunifTanjim/nui.nvim',
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            'rcarriga/nvim-notify',
+          },
+        },
+      },
 
       -- use a release tag to download pre-built binaries
       version = '1.*',
