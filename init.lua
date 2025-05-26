56a8bc6bc45b7f66a770aabfde763fa3172c75ba
Londo <ridecar2@gmail.com>
2025-05-26 03:30:39 +0100

    Stop blink.cmp working on the command line.

--- init.lua
+++ init.lua
@@ -808,11 +808,6 @@ require('lazy').setup(
       ---@module 'blink.cmp'
       ---@type blink.cmp.Config
       opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
         -- All presets have the following mappings:
         -- C-space: Open menu or open docs if already open
         -- C-n/C-p or Up/Down: Select next/previous item
@@ -821,6 +816,9 @@ require('lazy').setup(
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         keymap = { preset = 'enter' },
+
+        -- Disable command line completion as WSL is dog slow with it... shame really
+        cmdline = { enabled = false },
 
         appearance = {
           -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
