8b6b444bced749a27f0d759b92d2b75dfa6709b4
Londo <ridecar2@gmail.com>
2025-06-25 22:33:17 +0100

    Added AML syntax highlighting

--- init.lua
+++ init.lua
@@ -1151,6 +1151,7 @@ require('lazy').setup(
     -- require 'kickstart.plugins.neo-tree',
     -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+    { 'togglebyte/aml.vim' },
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
     --
