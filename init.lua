cffba85be66da993969809a2a1243a34518b55b6
LondoSpark <ridecar2@gmail.com>
2025-10-19 15:53:48 +0100

    Add and use the neovoid theme

--- init.lua
+++ init.lua
@@ -856,7 +856,6 @@ require('lazy').setup(
             -- OPTIONAL:
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
-            'rcarriga/nvim-notify',
           },
         },
       },
@@ -906,25 +905,6 @@ require('lazy').setup(
         fuzzy = { implementation = 'prefer_rust_with_warning' },
       },
       opts_extend = { 'sources.default' },
-    },
-    { -- You can easily change to a different colorscheme.
-      -- Change the name of the colorscheme plugin below, and then
-      -- change the command in the config to whatever the name of that colorscheme is.
-      --
-      -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-      'ellisonleao/gruvbox.nvim',
-      priority = 1000, -- Make sure to load this before all the other start plugins.
-      config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require('gruvbox').setup {
-          contrast = 'hard',
-        }
-
-        -- Load the colorscheme here.
-        -- Like many other themes, this one has different styles, and you could load
-        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'gruvbox'
-      end,
     },
 
     -- Highlight todo, notes, etc in comments
@@ -1108,6 +1088,8 @@ parser_config.html_eex = {
   },
   maintainers = { '@rockerBOO' },
 }
+
+vim.cmd.colorscheme 'neovoid'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
